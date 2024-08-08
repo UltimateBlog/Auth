5e1a88cc3d5d3d9b4c560bd9516c87b9ad0b3600
Reza Rajabi <rza.rajabi@gmail.com>
2024-08-08 17:05:08 +0330

    update services to return serializable objects

--- services/blogger_services.py
+++ services/blogger_services.py
@@ -2,28 +2,44 @@ from typing import Optional
 
 from nameko.rpc import rpc
 
+from db import Blogger
+from db.utils import convert_object_to_dict
+
 
 class BloggerService:
-    name = "bloger_service"
+    name = "blogger_service"
 
     @rpc
-    def get_blogger(self, blogger_id: int):
-        pass
+    def get_blogger(self, blogger_id: int, raise_: bool = False) -> Optional[dict]:
+        return convert_object_to_dict(Blogger.get(blogger_id, raise_not_found=raise_))
 
     @rpc
-    def get_bloggers(self, **query_params: dict) -> list:
-        pass
+    def get_bloggers(self, **filters: dict) -> list[dict]:
+        return convert_object_to_dict(Blogger.filter(**filters).all(), multi=True)
 
     @rpc
     def add_blogger(
-        self, firstname: str, lastname: str, email: str, username: Optional[str]
-    ):
-        pass
+        self,
+        firstname: str,
+        lastname: str,
+        email: str,
+        password: str,
+        username: Optional[str],
+    ) -> dict:
+        return convert_object_to_dict(
+            Blogger.insert(
+                firstname=firstname,
+                lastname=lastname,
+                email=email,
+                password=password,
+                username=username,
+            )
+        )
 
     @rpc
-    def delete_blogger(self, blogger_id: int):
-        pass
+    def delete_blogger(self, blogger_id: int) -> None:
+        Blogger.delete(blogger_id)
 
     @rpc
-    def update_blogger(self, blogger_id: int, **updated_params: dict):
-        pass
+    def update_blogger(self, blogger_id: int, **updated_params: dict) -> dict:
+        return convert_object_to_dict(Blogger.update(blogger_id, **updated_params))
