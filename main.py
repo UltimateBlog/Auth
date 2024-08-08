b724d0a228de208d6c8c5a6e7b5377ee6ba3fd18
Reza Rajabi <rza.rajabi@gmail.com>
2024-08-08 17:04:35 +0330

    run nameko service with python
    calling the nameko run command from main.py to make easier to run the services and other necessary command with a single command

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from db.startup import create_db_tables
+import subprocess
 
+from db.startup import create_db_tables
 
 if __name__ == "__main__":
     create_db_tables()
+    subprocess.call(["nameko", "run", "services.blogger_services"])
